062322cfd2ec4db792d8858a7f33526ab1f4dcfb
Jerry Tran <jerrytran.wrk@gmail.com>
2020-09-10 14:14:19 +0700

    oke

--- indexing.script.js
+++ indexing.script.js
@@ -1,35 +1,30 @@
 const fs = require("fs");
 
-function index(dataFolder) {
-  fs.readdir(dataFolder, (err, folders) => {
-    if (err) {
-      return;
-    }
-
-    folders.forEach((folder) => {
-      try {
-        index(folder);
-        const absolutePath = `${dataFolder}/${folder}`;
-        const temp = fs.readdirSync(absolutePath).map((report) => {
-          return `<a href="/${report}/index.html">Test ${folder}</a>\n`;
-        });
-        const indexFile = `${absolutePath}/index.html`;
-        const content = `<!DOCTYPE html>
-          <html dir="ltr">
-          <head>
-              <meta charset="utf-8">
-              <title>Allure Report</title>
-          </head>
-          <body>
-              ${temp.join("")}
-          </body>`;
-        fs.writeFile(indexFile, content, (err) => {
-          console.log(err);
-        });
-      } catch (error) {}
+function index(folder) {
+  try {
+    const temp = fs.readdirSync(folder).map((report) => {
+      const path = `${folder}/${report}`;
+      if (!fs.lstatSync(path).isDirectory()) {
+        return;
+      }
+      return `<div><a href="./${report}/index.html">Test ${report}</a></div>\n`;
     });
-  });
+    const indexFile = `${folder}/index.html`;
+    const content = `<!DOCTYPE html>
+        <html dir="ltr">
+        <head>
+            <meta charset="utf-8">
+            <title>Allure Report</title>
+        </head>
+        <body>
+            ${temp.join("")}
+        </body>`;
+    fs.writeFileSync(indexFile, content);
+  } catch (error) {}
 }
-
-const data = `${__dirname}`;
-index(data);
+const dir = `${__dirname}/data`;
+index(dir);
+fs.readdirSync(dir).forEach((name) => {
+  const folder = `${dir}/${name}`;
+  index(folder);
+});
